a0a238f9b9aa4c93ba54663ec44ad1b016c5bade
Vladimir Iakimov <yakimovvn@bk.ru>
2019-07-27 21:20:36 +0300

    Перенес свой чат. Используя Log4J настороил логирование. В отдельный файл грузятся ошибки. И файл со всеми сообщениями. В консоль все выводится через Log4J

---
+++ src/main/java/ChatFx/server/Server.java
@@ -0,0 +1,157 @@
+package ChatFx.server;
+
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class Server {
+    private Vector<ClientHandler> clients;
+    private Logger adminLog;
+
+    public Server() throws SQLException {
+        clients = new Vector<>();
+        ServerSocket server = null;
+
+        Socket socket = null;
+
+        adminLog = Logger.getLogger("admin");
+        try {
+            AuthService.connect();
+
+            server = new ServerSocket(8189);
+            writeToAdminLogInfo("Server is start");
+
+            while (true){
+                socket = server.accept();
+                writeToAdminLogInfo("Client connect");
+
+                new ClientHandler(socket,this);
+
+            }
+        }
+        catch (IOException e) {
+            writeToAdminLogFatal("Сервер упал!"+stackTraceToString(e.getStackTrace()));
+//            e.printStackTrace();
+        }
+        finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+    public boolean isNickBusy(String nick) {
+        for (ClientHandler o : clients) {
+            if (o.getNick().equals(nick)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void broadcastSystemMsg(String msg){
+        for (ClientHandler o: clients) {
+            o.sendMsg("/systemmsg "+msg);
+        }
+    }
+
+    public void broadcastMsg(ClientHandler from, String msg) {
+        for (ClientHandler o : clients) {
+            if (!AuthService.isInBlackList(from.getNick(),o.getNick())) {
+                o.sendMsg(msg);
+            }
+        }
+        writeToAdminLogDebug(String.format("%s: %s",from.getNick(),msg));
+
+    }
+
+    public void sendPersonalMsg(ClientHandler from, String nickTo, String msg) {
+        for (ClientHandler o : clients) {
+            if (o.getNick().equals(nickTo)) {
+                if(AuthService.isInBlackList(o.getNick(),from.getNick())){
+                    from.sendMsg("/wsystemmsg "+nickTo+" Вы находитесь в черном списке.");
+                    return;
+                }else {
+                    o.sendMsg("/w " + from.getNick() +" "+ from.getNick()+" " + msg);
+                    from.sendMsg("/w " + nickTo + " "+from.getNick()+ " " + msg);
+                    writeToAdminLogDebug(String.format("%s private message to %s: %s",from.getNick(),nickTo,msg));
+                    return;
+                }
+            }
+        }
+        from.sendMsg("/systemmsg Клиент с ником " + nickTo + " не в чате");
+    }
+
+    public void subscribe (ClientHandler clientHandler){
+        clients.add(clientHandler);
+        broadcastSystemMsg(clientHandler.getNick()+" подключился");
+        broadcastClientList();
+        AuthService.sendAllMessage(clientHandler);
+
+
+    }
+
+    public void unsubscribe (ClientHandler clientHandler){
+        clients.remove(clientHandler);
+        broadcastSystemMsg(clientHandler.getNick()+" отключился");
+        broadcastClientList();
+    }
+
+
+    public void broadcastClientList(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("/clientlist ");
+        for (ClientHandler clientHandler : clients) {
+           stringBuilder.append(clientHandler.getNick()) ;
+           stringBuilder.append(" ");
+        }
+        String out = stringBuilder.toString();
+        for (ClientHandler clientHandler: clients) {
+            clientHandler.sendMsg(out);
+        }
+
+    }
+
+    public void writeToAdminLogInfo(String logMessage){
+        adminLog.info(logMessage);
+    }
+
+    public void writeToAdminLogWarn(String logMessage){
+        adminLog.warn(logMessage);
+    }
+
+    public void writeToAdminLogDebug(String logMessage){
+        adminLog.debug(logMessage);
+    }
+
+    public void writeToAdminLogFatal(String logMessage){
+        adminLog.fatal(logMessage);
+    }
+
+    public void writeToAdminLogError(String logMessage){
+        adminLog.error(logMessage);
+    }
+
+    public String stackTraceToString(StackTraceElement[] stackTracesArr){
+        StringBuilder sb = new StringBuilder("\n");
+        for (StackTraceElement st : stackTracesArr) {
+            sb.append("\t");
+            sb.append(st.toString());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+
+}
