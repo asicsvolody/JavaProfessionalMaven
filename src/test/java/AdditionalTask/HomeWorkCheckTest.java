dc096f71296d9a393e9580610678c1c73729eae4
Vladimir Iakimov <yakimovvn@bk.ru>
2019-07-31 21:23:34 +0300

    Добавил в доп Д/з HashMap чтобы не плодить поля.

--- src/test/java/AdditionalTask/HomeWorkCheckTest.java
+++ src/test/java/AdditionalTask/HomeWorkCheckTest.java
@@ -17,6 +17,7 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 
 
@@ -32,7 +33,7 @@ public class HomeWorkCheckTest {
         ArrayList<File> filesArrList =new ArrayList<>();
         if(dir.isDirectory()){
             File[] filesArr = dir.listFiles();
-            for (File file: filesArr) {
+            for (File file: filesArr != null ? filesArr : new File[0]) {
                 if(file.getName().endsWith(".class")){
                     filesArrList.add(file);
                 }
@@ -41,6 +42,7 @@ public class HomeWorkCheckTest {
         printAllFilesName(filesArrList);
         return filesArrList;
     }
+
     private static void printAllFilesName(ArrayList<File> fileArrayList) {
         Iterator<File> iterator = fileArrayList.iterator();
         while (iterator.hasNext()){
@@ -54,45 +56,32 @@ public class HomeWorkCheckTest {
         this.classFile = classFile;
     }
 
-    Method calculateInt;
-    Method calculateFloat;
-    Method checkTwoNumbers;
-    Method printIsPositive;
-    Method isNegative;
-    Method printWelocome;
-    Method isLeapYear;
-    Class ch;
+    private Class ch;
+
+    private HashMap <String, Method> methodsHashMap;
 
 
-    ByteArrayOutputStream outputStream;
-    PrintStream old;
+    private ByteArrayOutputStream outputStream;
+    private PrintStream old;
 
 
     @Before
     public void init() throws MalformedURLException, ClassNotFoundException, NoSuchMethodException {
+        methodsHashMap = new HashMap<>();
         ch = URLClassLoader.newInstance(new URL[]{classFile.getParentFile().toURL()}).loadClass(getNameWithOutFormat(classFile.getName()));
 
-        calculateInt = ch.getDeclaredMethod("calculate",int.class,int.class, int.class,int.class);
-        calculateInt.setAccessible(true);
-        calculateFloat = ch.getDeclaredMethod("calculate",float.class,float.class,float.class,float.class);
-        calculateFloat.setAccessible(true);
-        checkTwoNumbers = ch.getDeclaredMethod("checkTwoNumbers",int.class, int.class);
-        checkTwoNumbers.setAccessible(true);
-        printIsPositive = ch.getDeclaredMethod("printIsPositive", int.class);
-        printIsPositive.setAccessible(true);
-        isNegative = ch.getDeclaredMethod("isNegative",int.class);
-        isNegative.setAccessible(true);
-        printWelocome = ch.getDeclaredMethod("printWelocome",String.class);
-        printWelocome.setAccessible(true);
-        isLeapYear = ch.getDeclaredMethod("isLeapYear",int.class);
-        isLeapYear.setAccessible(true);
+        methodsHashMap.put("calculateInt",ch.getDeclaredMethod("calculate",int.class,int.class, int.class,int.class));
+        methodsHashMap.put("calculateFloat",ch.getDeclaredMethod("calculate",float.class,float.class,float.class,float.class));
+        methodsHashMap.put("checkTwoNumbers",ch.getDeclaredMethod("checkTwoNumbers",int.class, int.class));
+        methodsHashMap.put("printIsPositive",ch.getDeclaredMethod("printIsPositive", int.class));
+        methodsHashMap.put("isNegative",ch.getDeclaredMethod("isNegative",int.class));
+        methodsHashMap.put("printWelocome",ch.getDeclaredMethod("printWelocome",String.class));
+        methodsHashMap.put("isLeapYear",ch.getDeclaredMethod("isLeapYear",int.class));
+        methodsHashMap.forEach((k,v)->v.setAccessible(true));
 
         old = System.out;
         outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
-
-
-
     }
 
     private String getNameWithOutFormat(String fileNameWithFormat){
@@ -110,34 +99,39 @@ public class HomeWorkCheckTest {
 
     @Test
     public void calculateIntTest() throws InvocationTargetException, IllegalAccessException {
-        Assert.assertEquals(44, (int)calculateInt.invoke(ch,4,8,6,2));
+        Assert.assertEquals(44, (int)methodsHashMap.get("calculateInt").invoke(ch,4,8,6,2));
     }
+
     @Test
     public void calculateFloatTest() throws InvocationTargetException, IllegalAccessException {
-        Assert.assertEquals(17.25f,(float)calculateFloat.invoke(ch,1.5F,2.5f,4.5f,0.5f),2);
+        Assert.assertEquals(17.25f,(float)methodsHashMap.get("calculateFloat").invoke(ch,1.5F,2.5f,4.5f,0.5f),2);
     }
+
     @Test
     public void checkTwoNumbersTest() throws InvocationTargetException, IllegalAccessException {
-        Assert.assertTrue((boolean)checkTwoNumbers.invoke(ch,7,8));
+        Assert.assertTrue((boolean)methodsHashMap.get("checkTwoNumbers").invoke(ch,7,8));
     }
+
     @Test
     public void printIsPositiveTest() throws InvocationTargetException, IllegalAccessException {
-        printIsPositive.invoke(ch,2);
+        methodsHashMap.get("printIsPositive").invoke(ch,2);
         Assert.assertEquals("Your number is positive!\n",outputStream.toString());
     }
+
     @Test
     public void isNegativeTest() throws InvocationTargetException, IllegalAccessException {
-        Assert.assertTrue((boolean)isNegative.invoke(ch,-2));
+        Assert.assertTrue((boolean)methodsHashMap.get("isNegative").invoke(ch,-2));
     }
 
     @Test
     public void printWelocomeTest() throws InvocationTargetException, IllegalAccessException {
-        printWelocome.invoke(ch,"Владимир");
+        methodsHashMap.get("printWelocome").invoke(ch,"Владимир");
         Assert.assertEquals("Привет, Владимир!\n",outputStream.toString());
     }
+
     @Test
     public void isLeapYearTest() throws InvocationTargetException, IllegalAccessException {
-        Assert.assertTrue((boolean)isLeapYear.invoke(ch,2012));
+        Assert.assertTrue((boolean)methodsHashMap.get("isLeapYear").invoke(ch,2012));
     }
 
     @After
