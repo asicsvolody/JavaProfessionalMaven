717ec5de30ed59c0424070f4b0bc97620974b025
Vladimir Iakimov <yakimovvn@bk.ru>
2019-08-01 22:41:28 +0300

    Убрал лишнее поле.

--- src/main/java/SpiralWithRecourse.java
+++ src/main/java/SpiralWithRecourse.java
@@ -1,6 +1,5 @@
 public class SpiralWithRecourse {
-    private int weight;
-    private int height;
+    private int maxNumber;
     private int horizontalMax;
     private int verticalMax;
     private int horizontalMin;
@@ -8,8 +7,7 @@ public class SpiralWithRecourse {
     private int number;
 
     private SpiralWithRecourse(int weight, int height) {
-        this.weight = weight;
-        this.height = height;
+        maxNumber = weight*height;
         horizontalMax = weight-1;
         verticalMax = height-1;
         horizontalMin = 0;
@@ -37,26 +35,26 @@ public class SpiralWithRecourse {
         }
         verticalMin++;
 
-        if(number >= weight*height) return;
+        if(number >= maxNumber) return;
 
         for (int i = verticalMin; i <= verticalMax; i++) {
             arr[i][horizontalMax] = number++;
         }
         horizontalMax--;
-        if(number >= weight*height) return;
+        if(number >= maxNumber) return;
 
 
         for (int i = horizontalMax; i >= horizontalMin; i--) {
             arr[verticalMax][i] = number++;
         }
         verticalMax--;
-        if(number >= weight*height) return;
+        if(number >= maxNumber) return;
 
         for (int i = verticalMax; i >= verticalMin; i--) {
             arr[i][horizontalMin] = number++;
         }
         horizontalMin++;
-        if(number >= weight*height) return;
+        if(number >= maxNumber) return;
 
         writeSpiralToArr(arr);
     }
